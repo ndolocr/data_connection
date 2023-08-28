1a04496130d79cd5c2d045c0f142444589713c2e
ndolocr <crndolo@gmail.com>
2023-08-28 12:56:17 +0300

    Create model for temporal data

--- temporal_account/models.py
+++ temporal_account/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from neomodel import(
+    Relationship,
+    StringProperty,
+    StructuredNode,
+    RelationshipTo,
+    RelationshipFrom,
+    UniqueIdProperty,
+    IntegerProperty,
+    ArrayProperty, 
+    DateProperty
+)
 # Create your models here.
+
+class TemporalData(StructuredNode):
+    UID = UniqueIdProperty()
+    ID_NO = StringProperty()
+    ACC_NO = StringProperty()
+    ACC_NAME = StringProperty()    
+    CREATED_ON = DateProperty()
